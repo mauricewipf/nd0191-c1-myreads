842f2a9de85301bd2613688fb93eaee595d9519a
Maurice Wipf <maurice@gmx.de>
2022-05-14 12:31:39 +0200

    feat: Retrieve books from API

--- starter/src/App.js
+++ starter/src/App.js
@@ -2,8 +2,20 @@ import "./App.css";
 import {Route, Routes} from "react-router-dom";
 import SearchPage from "./SearchPage";
 import MainPage from "./MainPage";
+import {useEffect, useState} from "react";
+import {getAll} from "./BooksAPI";
 
 function App() {
+
+    const [books, setBooks] = useState([]);
+
+    useEffect(() => {
+        const getBooks = async () => {
+            const res = await getAll();
+            setBooks(res);
+        };
+        getBooks();
+    }, []);
 
   return (
       <Routes>
