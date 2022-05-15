0bc1ac42c89aeb7e35e30b107432ec956b8e50f2
Maurice Wipf <maurice@gmx.de>
2022-05-15 09:32:30 +0200

    feat: Implement search function and display book results. Handle book results without thumbnail or author. Do not send request with empty query string

--- starter/src/SearchPage.js
+++ starter/src/SearchPage.js
@@ -1,6 +1,20 @@
 import {Link} from "react-router-dom";
+import {useState} from "react";
+import {search} from "./BooksAPI";
 
-const SearchPage = ({}) => {
+const SearchPage = () => {
+
+    const [query, setQuery] = useState("");
+    const [books, setBooks] = useState([]);
+
+    const updateQuery = async (query) => {
+        setQuery(query);
+        const results = query.length > 0
+            ? await search(query, 100)
+            : [];
+        setBooks(Array.isArray(results) ? results : []);
+    };
+
     return (
         <div className="search-books">
             <div className="search-books-bar">
@@ -9,11 +23,45 @@ const SearchPage = ({}) => {
                     <input
                         type="text"
                         placeholder="Search by title, author, or ISBN"
+                        value={query}
+                        onChange={(event) => updateQuery(event.target.value)}
                     />
                 </div>
             </div>
             <div className="search-books-results">
-                <ol className="books-grid"></ol>
+                <ol className="books-grid">
+                    {books.map((book) => (
+                        <li key={book.id}>
+                            <div className="book">
+                                <div className="book-top">
+                                    <div
+                                        className="book-cover"
+                                        style={{
+                                            width: 128,
+                                            height: 193,
+                                            backgroundImage: 'url(%s)'.replace('%s', book.imageLinks?.thumbnail)
+                                        }}
+                                    ></div>
+                                    <div className="book-shelf-changer">
+                                        <select value="none" onChange={ (event) => {} }>
+                                            <option value="none" disabled>
+                                                Move to...
+                                            </option>
+                                            <option value="currentlyReading">
+                                                Currently Reading
+                                            </option>
+                                            <option value="wantToRead">Want to Read</option>
+                                            <option value="read">Read</option>
+                                            <option value="none">None</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div className="book-title">{book.title}</div>
+                                <div className="book-authors">{book.authors?.join(", ")}</div>
+                            </div>
+                        </li>
+                    ))}
+                </ol>
             </div>
         </div>
     );
